681c8863be050bb351906d7fc03feb6bdc983f02
Elderrss <144640867+Elderrss@users.noreply.github.com>
2023-09-11 09:17:36 +0700

    Create Operasi_Aritmatika.cpp

---
+++ Operasi_Aritmatika.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace  std;
+
+int main ( ){
+      int a, b;
+      cout<<"PEMROGRAMAN OPERASI ARITMATIKA \n" ;
+      cout<<"############################### \n";
+      cout<<"Masukan angka pertama: ";
+      cin>>a ;
+      cout<< "Masukan angka kedua: ";
+      cin>>b ;
+      cout<<"Hasil Penjumlahan: "<<a + b<<endl;
+       cout<<"Hasil Pengurangan: "<<a - b<<endl;
+        cout<<"Hasil Perkalian: "<<a * b<<endl; cout<<"Hasil Pembagian: "<<a / b<<endl;
+      cout<<"Hasil Modulus: "<<a % b<<endl;
+       return 0;
+}
